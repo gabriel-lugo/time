cede69270230cec01b72ebf09ea5a23db08a1d30
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-15 09:54:12 +0100

    added function to button

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 window.addEventListener('DOMContentLoaded', main);
 
+let timeoutRef;
+
 function main() {
     // Start på programmet
 
-    setTimeout(changeBackgroundColor, 4000);
+    timeoutRef = setTimeout(changeBackgroundColor, 2000);
+    addEventListeners();
 }
 
 function changeBackgroundColor() {
     document.body.style.background = "lightblue"
+}
+
+function addEventListeners() {
+    const button = document.getElementById("cancel");
+    button.addEventListener('click', cancelBackgroundChange);
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeoutRef);
+
 }
 
 
