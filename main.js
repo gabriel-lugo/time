928efaccb6d20a174cbc1cce90c7f5a9269f7810
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2022-11-15 09:42:19 +0100

    added button

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-setTimeout(printHello, 5000) // Kör funktionen efter 1 sekund. "Callback"-funktion
+window.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    // Start på programmet
+
+    setTimeout(changeBackgroundColor, 4000);
+}
+
+function changeBackgroundColor() {
+    document.body.style.background = "lightblue"
+}
+
+
+
+
+// setTimeout(printHello, 5000) // Kör funktionen efter 1 sekund. "Callback"-funktion
 // setInterval(printHello, 2000, 3) // Kör funktionen varje 1 sekund
 
-console.log("Wait for it...")
+// console.log("Wait for it...")
 
-function printHello() {
-    console.log("Hello Friends!")
-}
+// function printHello() {
+    // console.log("Hello Friends!")
+// }
